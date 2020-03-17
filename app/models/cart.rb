4f1e3a3506f65e1f9948b06e124a726469fd4644
Enow Mbi <benowmbi@yahoo.com>
2020-03-17 15:49:26 +0100

    Modify cart model --Add total price method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,9 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cartships
   has_many :products, through: :cartships
+
+  def total_price
+    cartships.map{|cartship| cartship.quantity * cartship.product.price}.sum
+  end
+
 end
