04276519b7ca7c7dbd7972472167b561fc71def9
Enow Mbi <benowmbi@yahoo.com>
2020-03-17 14:37:14 +0100

    Add image using active_storage-- resize image in product model,whitelisted image in parameters,display image in show and index pages and added file_field in form partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :unit_price)
+      params.require(:product).permit(:name, :unit_price, :image)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  def thumbnail
+    return self.image.variant(resize: "200 x 175").processed
+  end
+
 end
