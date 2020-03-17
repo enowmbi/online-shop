4040833a02d5148044bc7ff239708aa677d8cff9
Enow Mbi <benowmbi@yahoo.com>
2020-03-17 14:56:14 +0100

    Add user to card migration and run migration

---
+++ db/migrate/20200317135511_add_user_to_cart.rb
@@ -0,0 +1,5 @@
+class AddUserToCart < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :carts, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_135328) do
+ActiveRecord::Schema.define(version: 2020_03_17_135511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_03_17_135328) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_03_17_135328) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
 end
