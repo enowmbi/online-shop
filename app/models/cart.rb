076b3fab9e21f61bc74dd32f880fcd7520503b2f
Enow Mbi <benowmbi@yahoo.com>
2020-03-17 15:12:06 +0100

    Add associations between cart,product and user -- Cart belongs to a User, Cart has many Cartships, and Cart has many products through Cartships , Product has many cartships and has many carts through cartships

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+  belongs_to :user
+  has_many :cartships
+  has_many :products, through: :cartships
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :cartships
+  has_many :carts, through: :cartship
 
   def thumbnail
     return self.image.variant(resize: "200 x 175").processed
