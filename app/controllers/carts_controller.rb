07b81a4bd0dbae0e7e6d8d82bfa28a3e0f70db3d
Enow Mbi <benowmbi@yahoo.com>
2020-03-19 01:21:20 +0100

    Modify carts_controller.rb --group products in cart by name

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
   def show
-    @cart = Cart.find(session[:cart_id])
+    @cart = Cart.find(session[:cart_id]).cartships.joins(:product).group(:name,:unit_price).count
   end
 end
