cc8cd0407bb1f2b03027bc80292bf050b499da87
Enow Mbi <benowmbi@yahoo.com>
2020-03-19 01:22:31 +0100

    Modify carts_heloper.rb --add methods to get product quantity,unit price,product name and extended price from cartship

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,21 @@
 module CartsHelper
+  def get_product_name(cartship)
+    cartship[0][0] 
+  end
+
+  def get_product_quantity(cartship)
+    cartship[1] 
+  end
+
+  def get_product_unit_price(cartship)
+    number_to_currency(cartship[0][1]) 
+  end
+
+  def get_extended_product_price(cartship)
+    number_to_currency(cartship[1] * cartship[0][1])
+  end
+
+  def get_total_amount_to_pay
+    number_to_currency(@cart.map{|cartship| cartship[0][1] * cartship[1] }.sum)
+  end
 end
