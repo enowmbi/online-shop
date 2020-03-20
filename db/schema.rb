8fc62aa13054adc3bdd2c33b564f1f8b83655aba
Enow Mbi <benowmbi@yahoo.com>
2020-03-20 07:38:29 +0100

    Modify schema.rb --add category_id to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_020141) do
+ActiveRecord::Schema.define(version: 2020_03_20_034153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2020_03_20_020141) do
     t.decimal "unit_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -95,4 +97,5 @@ ActiveRecord::Schema.define(version: 2020_03_20_020141) do
   add_foreign_key "cartships", "products"
   add_foreign_key "category_product_assignments", "categories"
   add_foreign_key "category_product_assignments", "products"
+  add_foreign_key "products", "categories"
 end
