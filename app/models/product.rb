0dc471396fe308463632655a7dd5bb22df7c0a95
Enow Mbi <benowmbi@yahoo.com>
2020-03-20 03:00:12 +0100

    Modify product model --add association with categories and category_product_assignents

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :cartships
   has_many :carts, through: :cartship
+  has_many :category_product_assignment
+  has_many :categories, through: :category_product_assignments
 
   def thumbnail
     return self.image.variant(resize: "200 x 175").processed
