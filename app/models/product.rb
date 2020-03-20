795ff44d6582b408a4b796bedcdccb3da174c099
Enow Mbi <benowmbi@yahoo.com>
2020-03-20 10:19:11 +0100

    Modify product model --change association from category_product_assignment to category_product_assignments

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :cartships
   has_many :carts, through: :cartship
-  has_many :category_product_assignment
+  has_many :category_product_assignments
   has_many :categories, through: :category_product_assignments
 
   def thumbnail
