658590168dd2a78e8b1c15a5378f3867d18f33fe
Enow Mbi <benowmbi@yahoo.com>
2020-03-20 11:10:09 +0100

    Modify product model --remove many-to-many associtions and add one-to-many association

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,7 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :cartships
   has_many :carts, through: :cartship
-  has_many :category_product_assignments
-  has_many :categories, through: :category_product_assignments
+  belongs_to :category
 
   def thumbnail
     return self.image.variant(resize: "200 x 175").processed
