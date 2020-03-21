b4329ad8d37aa093e16fb52e67270e7702c9fd00
Enow Mbi <benowmbi@yahoo.com>
2020-03-21 06:57:20 +0100

    Modify product model --formatted code in vim

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_many :cartships
   has_many :carts, through: :cartship
-  belongs_to :category,counter_cache: true
+  belongs_to :category, counter_cache: true
 
   def thumbnail
     return self.image.variant(resize: "200 x 175").processed
