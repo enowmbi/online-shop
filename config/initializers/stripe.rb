88a11b5f127bd7cd1b43dc28afa55ce567cb4c3c
Enow Mbi <benowmbi@yahoo.com>
2020-03-26 04:15:35 +0100

    add stripe.rb to config/initializers

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+Rails.configuration.stripe = {
+  :publishable_key => Rails.application.credentials.stripe.dig(:publishable_key),
+  :secret_key      => Rails.application.credentials.stripe.dig(:secret_key)
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
