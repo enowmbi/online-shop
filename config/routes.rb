94345b36e82b845e157550f8e1393ee38492870e
Enow Mbi <benowmbi@yahoo.com>
2020-03-28 09:14:56 +0100

    Modify routes.rb --add member method pay of type post to cart

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
       get :add_to_cart
     end
   end
-  resources :carts, only: :show
+  resources :carts, only: :show do 
+    member do
+      post :pay
+    end
+  end
   resources :categories do 
     member do 
       get :products
