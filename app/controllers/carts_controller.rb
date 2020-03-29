10806c11880b6e538ea84325aca0e383445b0dbd
Enow Mbi <benowmbi@yahoo.com>
2020-03-29 05:40:42 +0100

    Modify carts_controller --add exception handling to show action --move redirect and status update of cart before the rescue block of pay action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,12 @@
 class CartsController < ApplicationController
   def show
-    @cart = Cart.find(session[:cart_id]).cartships.joins(:product).group(:name,:unit_price).count
-    @cart_id = session[:cart_id]
-    @cart_summary = Cart.find(session[:cart_id])
+    begin
+      @cart = Cart.find(session[:cart_id]).cartships.joins(:product).group(:name,:unit_price).count
+      @cart_id = session[:cart_id]
+      @cart_summary = Cart.find(session[:cart_id])
+    rescue
+      redirect_to root_path,notice: "Selected cart is no longer active, probably because it has been paid already"
+    end
   end
 
   def pay
@@ -20,16 +24,18 @@ class CartsController < ApplicationController
       currency: 'usd'
     )
 
+    @cart = Cart.find(session[:cart_id])
+    @cart.update(status: "Paid")
+    session.delete(:cart_id)
+    redirect_to root_path,notice: "Payment accepted -- Items are on their way"
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
+    puts "#{flash[:error]}"
+    redirect_to cart_path(Cart.find(session[:cart_id]))
     redirect_to new_charge_path
 
-   @cart = Cart.find(session[:cart_id])
-   @cart.update(status: "Paid")
-   session.delete(:cart_id)
-   redirect_to root_path,notice: "Payment accepted -- Items are on their way"
-
+    puts "running code after rescue !!!"
   end    
 
 end
