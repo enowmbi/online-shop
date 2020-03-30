3fbf038a0def0c7100f76bc9e0075da948a0cc59
Enow Mbi <benowmbi@yahoo.com>
2020-03-30 17:43:25 +0100

    Modify development.rb --setup configuration for sending email by smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: 'plain',
+    domain: 'localhost.com',
+    user_name: Rails.application.credentials.dig(:user_name),
+    password: Rails.application.credentials.dig(:password),
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
