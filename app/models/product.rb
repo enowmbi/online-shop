eb3fdc2207951e73735ed94cf554d0fcdda3e5b9
Enow Mbi <benowmbi@yahoo.com>
2020-03-31 14:18:43 +0100

    Modify product.rb --add association(has_many :reviews)

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :cartships
   has_many :carts, through: :cartship
   belongs_to :category, counter_cache: true
+  has_many :reviews
 
   def thumbnail
     return self.image.variant(resize: "200 x 175").processed
