90a31357caa24af92e0c28c75dc2f4a2b7515f4b
Enow Mbi <benowmbi@yahoo.com>
2020-03-31 11:30:26 +0100

    Modify routes.rb --add reviews resources nested under product

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/edit'
   root to: 'categories#index'
   devise_for :users
   resources :users, only: :show
   resources :products do 
     member do
       get :add_to_cart
+      resources :reviews, only: [:new,:edit]
     end
   end
   resources :carts, only: :show do 
