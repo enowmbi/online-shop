8d6ada85684034391a0f543187bd792527e8cacd
Enow Mbi <benowmbi@yahoo.com>
2020-03-31 16:10:59 +0100

    Modify reviews_controller --add new and create actions and whitelist of parameters

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,44 @@
 class ReviewsController < ApplicationController
+
   def new
+    @review = Review.new
+    @product = Product.find(params[:product_id])
   end
 
-  def edit
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+
+    if @review.save 
+      redirect_to product_path(params[:product_id]),notice: "Product review was saved"
+    else
+      render :new
+    end
   end
+
+  def edit 
+
+
+  end 
+
+  def update
+
+  end
+
+  private 
+  def set_product
+
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment,:rating, :product_id)
+  end
+
+
 end
+
