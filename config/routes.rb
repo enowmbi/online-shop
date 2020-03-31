4a77c8af0825f450e5937852445ba0b6c0a11803
Enow Mbi <benowmbi@yahoo.com>
2020-03-31 13:38:30 +0100

    Modify routes.rb --restore resources for reviews without whitelisted actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :products do 
     member do
       get :add_to_cart
-      resources :reviews, only: [:new,:edit]
+      resources :reviews
     end
   end
   resources :carts, only: :show do 
