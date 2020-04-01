a5c1845cd3ebcddafb93a7d56280a7cad708a533
Enow Mbi <benowmbi@yahoo.com>
2020-04-01 01:49:48 +0100

    Modify reviews_controller.rb --change redirect_to to use @review.product instead of  params[:product_id]

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_product, only: [:new, :edit]
   def new
     @review = Review.new
-    @product = Product.find(params[:product_id])
   end
 
   def create
@@ -11,24 +12,31 @@ class ReviewsController < ApplicationController
     @review.product_id = params[:product_id]
 
     if @review.save 
-      redirect_to product_path(params[:product_id]),notice: "Product review was saved"
+      redirect_to product_path(@review.product),notice: "Product review was saved"
     else
       render :new
     end
   end
 
   def edit 
-
-
   end 
 
   def update
+    if @review.update(review_params)
+      redirect_to product_path(@review.product),notice: "Review was successfully updated"
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+     @review.destroy
+     redirect_to product_path(@review.product),alert: "Review has been destroyed"
   end
 
   private 
   def set_product
-
+    @product = Product.find(params[:product_id])
   end
 
   def set_review
