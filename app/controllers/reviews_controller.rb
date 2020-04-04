b9aca4ae607a15a805ef0180965d40480e55d662
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 10:05:25 +0100

    Modify reviews_controller --modify before_action :set_product to include create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
 
   before_action :set_review, only: [:edit, :update, :destroy]
-  before_action :set_product, only: [:new, :edit]
+  before_action :set_product, only: [:new, :create, :edit]
   def new
     @review = Review.new
   end
