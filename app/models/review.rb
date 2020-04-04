3fc9a1eb0b78b9d79993c369b5a299a4f744bfd7
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 11:13:47 +0100

    Modify product.rb --add counter_cache: true to belongs_to method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :product
+  belongs_to :product,counter_cache: true
 
   validates :user_id, uniqueness: {scope: :product_id,message: "You are entitled to only one review per product"}
 
