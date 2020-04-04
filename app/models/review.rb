22414769ca1538a7873f0b6562aeb92c00977da9
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 10:03:23 +0100

    Modify review model --validate uniqueness of user_id within the scope of product_id meaning a user can only have one review for a product

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :user_id, uniqueness: {scope: :product_id,message: "You are entitled to only one review per product"}
+
 end
