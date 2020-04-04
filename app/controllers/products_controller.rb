e46ca780105ea436822e6ceb280ff4e1a74b3ab3
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 14:19:47 +0100

    Modify products_controller --includes reviews with product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -78,7 +78,7 @@ class ProductsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_product
-    @product = Product.find(params[:id])
+    @product = Product.includes(:reviews).find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
