72b3a75529090ab768f3ce42a46c74bdba57ab5a
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 14:10:08 +0100

    Modify products_controller --use averate_rating from model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @average_rating = @product.average_rating
   end
 
   # GET /products/new
