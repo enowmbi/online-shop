a0464786e0b72cf63fb4095b30571b8849eb0257
Enow Mbi <benowmbi@yahoo.com>
2020-04-04 05:44:44 +0100

    Modify payment_notification_job.rb --call deliver method to deliver mail in the perform method

--- app/jobs/payment_notification_job.rb
+++ app/jobs/payment_notification_job.rb
@@ -2,6 +2,6 @@ class PaymentNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    NotificationMailer.successful_payment_notification(args)
+    NotificationMailer.successful_payment_notification(args[0]).deliver
   end
 end
