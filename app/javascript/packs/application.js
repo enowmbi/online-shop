f70701be48c5f9c5bbf14f6649d555441dccecc9
Enow Mbi <benowmbi@yahoo.com>
2020-04-07 21:48:04 +0100

    Modify application.js --import bootstrap,jquery,popper.js and raty-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,6 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import "bootstrap"
-import "jquery"
-import "popper.js"
+import "bootstrap";
+import "jquery";
+import "popper.js";
+import "raty-js";
