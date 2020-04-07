5db85b9687e76b68af8925c07fabda31b29ca817
Enow Mbi <benowmbi@yahoo.com>
2020-04-07 23:32:44 +0100

    Modify review.rb --remove validation of number_field

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,9 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product,counter_cache: true
 
-  validates :user_id, uniqueness: {scope: :product_id,message: "You are entitled to only one review per product"}
+  validates :comment, presence: true
+
+  validates :user_id, uniqueness: {scope: :product_id, message: "You are entitled to only one review per product"}
 
   default_scope ->{order(updated_at: :desc)}
 
