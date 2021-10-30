97885affd04faad0d953f4a55d71f602bde74b4d
Enow Mbi <benowmbi@yahoo.com>
2021-10-30 15:03:31 +0100

    Create worker process using sidekiq background processor

---
+++ app/workers/unsuccessful_payment_notification_worker.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# sends mail information user of unsuccessful payment
+class UnsuccesfulPaymentNotificationWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :unsuccessful_payment_notification, retry: 3
+
+  def perform(cart)
+    NotificationMailer.unsuccessful_payment_notification(cart).deliver_async
+  rescue StandardException => e
+    # TODO: setup sentry to logging and reporting errors in real time
+    # Sentry.capture_exception(e)
+
+    Rails.logger.error("Error in UnsuccessfulPaymentNotificationWorker -#{e.message}")
+  end
+end
