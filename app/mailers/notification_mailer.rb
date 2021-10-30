ca27f7af17cbecfe3585ebb4a657c8c466cfd1ce
Enow Mbi <benowmbi@yahoo.com>
2021-10-30 14:21:00 +0100

    Refactor notification mailer methods -- add subject in mail method

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,15 +2,15 @@ class NotificationMailer < ApplicationMailer
 
   def successful_payment_notification(cart)
     @greeting = "Dear #{cart.user.email}"
-    @subject = "Successful Payment"
+    subject = "Successful Payment"
     @cart_id = cart.id
-    mail to: 'benowmbi@yahoo.com'
+    mail(to: 'benowmbi@yahoo.com', subject: subject)
   end
 
   def failed_payment_notification(cart)
     @greeting = "Dear #{cart.user.email}"
-    @subject = "Payment was not successful"
+    subject = "Payment was not successful"
     @cart_id = cart.id
-    mail to: cart.user.email
+    mail(to: cart.user.email, subject: subject)
   end
 end
