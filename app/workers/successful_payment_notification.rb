3cf3b73fb93a25436bebb0bc0201338ab88a994f
Enow Mbi <benowmbi@yahoo.com>
2021-11-01 07:58:17 +0100

    Use custom queue names in notification workers

---
+++ app/workers/successful_payment_notification.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# sends mail information user of unsuccessful payment
+class SuccessfulPaymentNotificationWorker
+  include Sidekiq::Worker
+  # sidekiq_options queue: :successful_payment_notification, retry: 3
+  sidekiq_options queue: :default, retry: 3
+
+  def perform(cart)
+    NotificationMailer.successful_payment_notification(cart).deliver_later
+  rescue StandardException => e
+    # TODO: setup sentry to logging and reporting errors in real time
+    # Sentry.capture_exception(e)
+
+    Rails.logger.error("Error in SuccessfulPaymentNotificationWorker -#{e.message}")
+  end
+end
